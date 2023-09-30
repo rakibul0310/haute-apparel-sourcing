b225d47250fac0ba149cda0478238844e8964436
rakibul2180@gmail.com <rakibul2180@gmail.com>
2023-09-30 23:45:15 +0600

    uni data added

---
+++ src/data/products.js
@@ -0,0 +1,129 @@
+import uni1 from "../assets/images/uniform/uni1.jpeg";
+import uni2 from "../assets/images/uniform/uni2.webp";
+import uni3 from "../assets/images/uniform/uni03webp.webp";
+import uni4 from "../assets/images/uniform/uni04.webp";
+import uni5 from "../assets/images/uniform/uni05.jpeg";
+import uni6 from "../assets/images/uniform/uni06.webp";
+import uni7 from "../assets/images/uniform/uni07.jpeg";
+import uni8 from "../assets/images/uniform/uni08.jpeg";
+import uni9 from "../assets/images/uniform/uni09.jpeg";
+import uni10 from "../assets/images/uniform/uni10.jpeg";
+import uni11 from "../assets/images/uniform/uni11.jpeg";
+import uni12 from "../assets/images/uniform/uni12.jpeg";
+import uni13 from "../assets/images/uniform/uni13.jpeg";
+import uni14 from "../assets/images/uniform/uni14.jpeg";
+import uni15 from "../assets/images/uniform/uni15.jpeg";
+import uni16 from "../assets/images/uniform/uni16.jpeg";
+import uni17 from "../assets/images/uniform/uni17.jpeg";
+import uni18 from "../assets/images/uniform/uni18.jpeg";
+
+export const uniform = [
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni1,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni2,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni3,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni4,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni5,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni6,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni7,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni8,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni9,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni10,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni11,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni12,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni13,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni14,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni15,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni16,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni17,
+  },
+  {
+    title: "School Uniform",
+    subTitle: "100% cotton",
+    category: "All",
+    image: uni18,
+  },
+];
